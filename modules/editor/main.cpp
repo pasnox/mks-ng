4050169cdeea37956b850236a59793d96bf208c5
Nox P@sNox <pasnox@gmail.com>
2012-05-13 01:53:19 +0200

    Initialize application informations

--- modules/editor/main.cpp
+++ modules/editor/main.cpp
@@ -1,5 +1,8 @@
 #include <QApplication>
 
+#include <FreshCore/pSettings>
+
+#include "main.h"
 #include "UIMain.h"
 
 #include "Abstractors.h"
@@ -8,10 +11,19 @@
 int main( int argc, char** argv )
 {
     QApplication app( argc, argv );
+    QApplication::setApplicationName( APPLICATION_NAME );
+    QApplication::setApplicationVersion( APPLICATION_VERSION );
+    QApplication::setOrganizationDomain( APPLICATION_DOMAIN );
+    QApplication::setOrganizationName( APPLICATION_ORGANIZATION );
+    QApplication::setWindowIcon( QIcon( ":/fresh/icons/info.png" ) );
+    
+    pSettings::setDefaultProperties( pSettings::Properties( APPLICATION_NAME, APPLICATION_VERSION, pSettings::Auto ) );
     
     Abstractors::add( Abstractors::CodeEditor, &SourceHighlightQtCodeEditor::staticMetaObject );
+    //Abstractors::add( Abstractors::CodeEditor, &QScintillaCodeEditor::staticMetaObject );
     
     UIMain w;
+    w.setWindowTitle( QString( "%1 v%2 (%3)[*]" ).arg( APPLICATION_NAME ).arg( APPLICATION_VERSION ).arg( APPLICATION_VERSION_LONG ) );
     w.showMaximized();
     
     return app.exec();
