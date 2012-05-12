37dacde7f01cd03c3537cbe2986f4260c550d0d1
Nox P@sNox <pasnox@gmail.com>
2012-05-13 01:42:55 +0200

    Add menu and main headers for reusable definitions.

---
+++ modules/editor/Menu.h
@@ -0,0 +1,19 @@
+#ifndef MENU_H
+#define MENU_H
+
+#define MENU_FILE "mFile"
+#define MENU_FILE_OPEN "mFile/aOpen"
+#define MENU_FILE_OPEN_PLAIN_TEXT "mFile/aOpenPlainText"
+#define MENU_FILE_SEPARATOR_1 "mFile/aSeparator1"
+#define MENU_FILE_RELOAD "mFile/aReload"
+#define MENU_FILE_SEPARATOR_2 "mFile/aSeparator2"
+#define MENU_FILE_SAVE "mFile/aSave"
+#define MENU_FILE_SAVE_ALL "mFile/aSaveAll"
+#define MENU_FILE_BACKUP "mFile/aBackup"
+#define MENU_FILE_SEPARATOR_3 "mFile/aSeparator3"
+#define MENU_FILE_CLOSE "mFile/aClose"
+#define MENU_FILE_CLOSE_ALL "mFile/aCloseAll"
+#define MENU_FILE_SEPARATOR_4 "mFile/aSeparator4"
+#define MENU_FILE_QUIT "mFile/aQuit"
+
+#endif // MENU_H
---
+++ modules/editor/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define APPLICATION_NAME "Monkey Studio NG"
+#define APPLICATION_VERSION "0.1.0"
+#define APPLICATION_VERSION_LONG "0.1.0git"
+#define APPLICATION_DOMAIN "monkeystudio.org"
+#define APPLICATION_ORGANIZATION "SoDream"
+
+#endif // MAIN_H
