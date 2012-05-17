8f51e4e490fe6c7156efc0f8d2a6258b61381974
Nox P@sNox <pasnox@gmail.com>
2012-05-17 13:11:59 +0200

    Concert the init-repositories script to python so it's cross platform.

--- init-repositories.py
+++ init-repositories.py
@@ -2,20 +2,23 @@
 
 import sys
 import os
-import stat
-import fileinput
+import platform
+import shlex
+import subprocess
 
 class RepositoryInitializer:
     # errors
+    EC_OK = 0
     EC_CANT_CREATE_PATH = 1
     EC_CANT_EXPORT = 2
     EC_CANT_UPDATE = 3
+
     # the modules to clone / check out
     modules = {
         'git':
             {
-                'src_highlite': 'git://git.savannah.gnu.org/src-highlite.git',
-                'src_highlite_qt': 'git://srchiliteqt.git.sourceforge.net/gitroot/srchiliteqt/srchiliteqt',
+                'source-highlight': 'git://git.savannah.gnu.org/src-highlite.git',
+                'source-highlight-qt': 'git://srchiliteqt.git.sourceforge.net/gitroot/srchiliteqt/srchiliteqt',
                 'fresh': 'git://github.com/pasnox/fresh.git'
             },
         'svn':
@@ -23,49 +26,99 @@ class RepositoryInitializer:
                 'ctags': 'https://ctags.svn.sourceforge.net/svnroot/ctags/trunk'
             }
     }
-    
-    def __init__(self):
-        os.environ[ 'PATH' ] = '/opt/local/bin:/opt/bin:%s' % ( os.environ[ 'PATH' ] )
 
-    # $1 system
-    # $2 uri
-    # $3 folder
-    def systemExport(self, cvs, uri, folder)
-    {
-        if [ "$1" == "git" ] ; then
-            if [ -z "$3" ] ; then
-                git clone "$2" || exit $EC_CANT_EXPORT
-            else
-                git clone "$2" "$3" || exit $EC_CANT_EXPORT
-            fi
-        elif [ "$1" == "svn" ] ; then
-            if [ -z "$3" ] ; then
-                svn co -q "$2" || exit $EC_CANT_EXPORT
-            else
-                svn co -q "$2" "$3" || exit $EC_CANT_EXPORT
-            fi
-        fi
-    }
+    def __init__(self):
+        if not platform.system() == 'Windows':
+            os.environ[ 'PATH' ] = '/opt/local/bin:/opt/bin:%s' % ( os.environ[ 'PATH' ] )
+
+    def execute(self, command, workingDirectory = None, showError = False, showExecInfo = False, usePipe = False):
+        if showExecInfo:
+            if workingDirectory:
+                print ' - From: %s' % ( workingDirectory )
+            print ' - Executing: %s' % command
+        args = shlex.split( command )
+        if usePipe:
+            runner = subprocess.Popen( args, cwd = workingDirectory, stdout = subprocess.PIPE, stderr= subprocess.STDOUT, shell = False )
+        else:
+            runner = subprocess.Popen( args, cwd = workingDirectory, shell = False )
+        output, error = runner.communicate()
+        if runner.returncode != 0:
+            if showError:
+                print ' - Exit Code: %i' % ( runner.returncode )
+                print output
+            return False
+        return True
+
+    def thirdPartyFolderFilePath(self, folderName):
+        thirdPartyName = '3rdparty'
+        if not os.path.exists( thirdPartyName ):
+            os.makedirs( thirdPartyName )
+            if not os.path.exists( thirdPartyName ):
+                return RepositoryInitializer.EC_CANT_CREATE_PATH
+        return '%s/%s' % ( thirdPartyName, folderName )
+
+    def systemUpdate(self, cvs, folder = None):
+        if cvs == "git":
+            if folder:
+                if not self.execute( 'git pull "%s"' % ( folder ) ):
+                    return RepositoryInitializer.EC_CANT_UPDATE
+            else:
+                if not self.execute( 'git pull' ):
+                    return RepositoryInitializer.EC_CANT_UPDATE
+        elif cvs == "svn":
+            if folder:
+                if not self.execute( 'svn update "%s"' % ( folder ) ):
+                    return RepositoryInitializer.EC_CANT_UPDATE
+            else:
+                if not self.execute( 'svn update' ):
+                    return RepositoryInitializer.EC_CANT_UPDATE
+        return RepositoryInitializer.EC_OK
+
+    def systemExport(self, cvs, uri, folder):
+        if cvs == "git":
+            if folder:
+                if not self.execute( 'git clone "%s" "%s"' % ( uri, folder ) ):
+                    return RepositoryInitializer.EC_CANT_EXPORT
+            else:
+                if not self.execute( 'git clone "%s"' % ( uri ) ):
+                    return RepositoryInitializer.EC_CANT_EXPORT
+        elif cvs == "svn":
+            if folder:
+                if not self.execute( 'svn co -q "%s" "%s"' % ( uri, folder ) ):
+                    return RepositoryInitializer.EC_CANT_EXPORT
+            else:
+                if not self.execute( 'svn co -q "%s"' % ( uri ) ):
+                    return RepositoryInitializer.EC_CANT_EXPORT
+        return RepositoryInitializer.EC_OK
 
     def moduleExport(self, cvs, module, uri):
         print '--- Updating module %s...' % ( module )
         
-        folder = '' #`thirdPartyFolderFilePath "$2.$1"`
+        folder = self.thirdPartyFolderFilePath( '%s.%s' % ( module, cvs ) )
+        
+        if folder == RepositoryInitializer.EC_CANT_CREATE_PATH:
+            return folder
         
         if not os.path.isdir( folder ):
-            systemExport( cvs, uri, folder )
+            result = self.systemExport( cvs, uri, folder )
+            if result != RepositoryInitializer.EC_OK:
+                return result
         
         cwd = os.getcwd();
         os.chdir( folder )
-        systemUpdate( cvs );
+        result = self.systemUpdate( cvs );
         os.chdir( cwd )
-    
+        return result
+
     def run(self):
         for cvs in RepositoryInitializer.modules.keys():
             cvsModules = RepositoryInitializer.modules[ cvs ]
             for module in cvsModules.keys():
                 uri = cvsModules[ module ]
-                moduleExport( cvs, module, uri )
+                result = self.moduleExport( cvs, module, uri )
+                if result != RepositoryInitializer.EC_OK:
+                    return result
+        return RepositoryInitializer.EC_OK
 
 def main(argv=None):
     if argv is None:
@@ -76,50 +129,3 @@ def main(argv=None):
 
 if __name__ == "__main__":
     sys.exit( main() )
-
-
-    
-'''
-#################################### START FUNCTIONS#####################################
-
-# avoid the print of pushd stack
-pushd()
-{
-    builtin pushd "$@" > /dev/null;
-}
-
-# $1 folder name
-thirdPartyFolderFilePath()
-{
-    _thirdPartyName="3rdparty"
-    
-    if [ ! -d "$_thirdPartyName" ] ; then
-        mkdir -p "$_thirdPartyName" || exit $EC_CANT_CREATE_PATH
-    fi
-    
-    echo "$_thirdPartyName/$1"
-}
-
-
-
-# $1 system
-# $2 folder
-systemUpdate()
-{
-    if [ "$1" == "git" ] ; then
-        if [ -z "$2" ] ; then
-            git pull || exit $EC_CANT_UPDATE
-        else
-            git pull "$2" || exit $EC_CANT_UPDATE
-        fi
-    elif [ "$1" == "svn" ] ; then
-        if [ -z "$2" ] ; then
-            svn update || exit $EC_CANT_UPDATE
-        else
-            svn update "$2" || exit $EC_CANT_UPDATE
-        fi
-    fi
-}
-
-
-'''
