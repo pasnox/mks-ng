98da35e38b54039b64a6a292641656d1a7ab0603
Nox P@sNox <pasnox@gmail.com>
2012-06-05 21:13:04 +0200

    Add support for languageForMimeType.

--- modules/editor/abstractors/codeeditor/SourceHighlightQt/SourceHighlightQtCodeEditor.cpp
+++ modules/editor/abstractors/codeeditor/SourceHighlightQt/SourceHighlightQtCodeEditor.cpp
@@ -36,7 +36,7 @@ QIcon SourceHighlightQtCodeEditor::iconForLanguage( const QString& language ) co
         mimeTypes[ "style" ] = "";
         //
         mimeTypes[ "ada" ] = "text/x-adasrc";
-        mimeTypes[ "applescript" ] = QString::null;
+        mimeTypes[ "applescript" ] = "";
         mimeTypes[ "asm" ] = "text/x-nasm";
         mimeTypes[ "awk" ] = "application/x-awk";
         mimeTypes[ "bat" ] = "application/x-shellscript";
@@ -147,30 +147,102 @@ QIcon SourceHighlightQtCodeEditor::iconForLanguage( const QString& language ) co
 
 QString SourceHighlightQtCodeEditor::languageForMimeType( const QString& type ) const
 {
-#if defined( HAS_QT_5 )
-    /*const QStringList languages = supportedLanguages();
+    static QHash<QString, QString> mimeTypes;
     
-    foreach ( const QString& language, languages ) {
-        const QString name = language.section( '.', 0, 0 );
-        
-        if ( type.suffixes().contains( name ) ) {
-            return language;
-        }
-        
-        if ( type.name().contains( "hdr" ) ) {
-            const QString srcName = type.name().replace( "hdr", "src" );
-            const QMimeType srcType = Abstractors::mimeDatabase().mimeTypeForName( srcName );
-            
-            if ( srcType.suffixes().contains( name ) ) {
-                return language;
-            }
-        }
-    }*/
-#else
-    Q_UNUSED( type );
-#endif
-
-    return QString::null;
+    if ( mimeTypes.isEmpty() ) {
+        // specials
+        /*mimeTypes[ "" ] = "default";
+        mimeTypes[ "" ] = "langdef";
+        mimeTypes[ "" ] = "nohilite";
+        mimeTypes[ "" ] = "outlang";
+        mimeTypes[ "" ] = "style";*/
+        //
+        mimeTypes[ "text/x-adasrc" ] = "ada";
+        //mimeTypes[ "" ] = "applescript";
+        mimeTypes[ "text/x-nasm" ] = "asm";
+        mimeTypes[ "application/x-awk" ] = "awk";
+        mimeTypes[ "application/x-shellscript" ] = "bat";
+        mimeTypes[ "text/x-bibtex" ] = "bib";
+        mimeTypes[ "" ] = "bison";
+        mimeTypes[ "text/x-chdr" ] = "c"; // c_comment, c_string
+        mimeTypes[ "text/x-csrc" ] = "c"; // c_comment, c_string, clike_vardeclaration
+        mimeTypes[ "text/x-objchdr" ] = "c";
+        mimeTypes[ "text/x-objcsrc" ] = "c";
+        mimeTypes[ "text/x-ocaml" ] = "caml";
+        mimeTypes[ "text/x-changelog" ] = "changelog";
+        //mimeTypes[ "" ] = "clipper";
+        mimeTypes[ "text/x-cobol" ] = "cobol";
+        mimeTypes[ "text/x-ini" ] = "conf";
+        mimeTypes[ "text/x-c++hdr" ] = "cpp";
+        mimeTypes[ "text/x-c++src" ] = "cpp";
+        mimeTypes[ "text/x-csharp" ] = "csharp";
+        mimeTypes[ "text/css" ] = "css";
+        mimeTypes[ "text/x-dsrc" ] = "d";
+        mimeTypes[ "application/x-desktop" ] = "desktop";
+        mimeTypes[ "text/x-patch" ] = "diff";
+        mimeTypes[ "text/x-erlang" ] = "erlang";
+        //mimeTypes[ "" ] = "errors";
+        //mimeTypes[ "" ] = "extreme_*"; // extreme_comment, extreme_comment2, extreme_comment3, extreme_comment4
+        mimeTypes[ "text/x-fortran" ] = "*fortran"; // fixed-fortran, fortran
+        //mimeTypes[ "" ] = "flex";
+        //mimeTypes[ "" ] = "function";
+        mimeTypes[ "application/x-glsl" ] = "glsl";
+        mimeTypes[ "text/x-haskell" ] = "haskell";
+        mimeTypes[ "text/x-literate-haskell" ] = "haskell_literate";
+        mimeTypes[ "text/x-haxe" ] = "haxe";
+        mimeTypes[ "text/html" ] = "html"; // html_simple, html
+        mimeTypes[ "text/x-emacs-lisp" ] = "islisp";
+        mimeTypes[ "text/x-java" ] = "java";
+        mimeTypes[ "text/x-java" ] = "javalog";
+        mimeTypes[ "application/javascript" ] = "javascript";
+        //mimeTypes[ "" ] = "key_string";
+        mimeTypes[ "text/x-tex" ] = "latex";
+        mimeTypes[ "inode/vnd.kde.service.ldap" ] = "ldap";
+        mimeTypes[ "text/x-lilypond" ] = "lilypond";
+        mimeTypes[ "text/x-emacs-lisp" ] = "lisp";
+        mimeTypes[ "text/x-log" ] = "log";
+        mimeTypes[ "text/x-logtalk" ] = "logtalk";
+        //mimeTypes[ "" ] = "lsm";
+        mimeTypes[ "text/x-lua" ] = "lua";
+        mimeTypes[ "application/x-m4" ] = "m4";
+        mimeTypes[ "text/x-makefile" ] = "makefile";
+        mimeTypes[ "text/cache-manifest" ] = "manifest";
+        //mimeTypes[ "" ] = "number";
+        mimeTypes[ "text/x-opa" ] = "opa";
+        mimeTypes[ "application/x-oz-application" ] = "oz";
+        mimeTypes[ "text/x-pascal" ] = "pascal";
+        //mimeTypes[ "" ] = "pc";
+        mimeTypes[ "application/x-perl" ] = "perl";
+        mimeTypes[ "application/x-php" ] = "php";
+        mimeTypes[ "text/x-gettext-translation" ] = "po";
+        mimeTypes[ "application/postscript" ] = "postscript";
+        mimeTypes[ "text/x-prolog" ] = "prolog";
+        mimeTypes[ "text/x-ini" ] = "properties";
+        mimeTypes[ "application/x-protobuf" ] = "proto"; // not official
+        mimeTypes[ "text/x-python" ] = "python";
+        mimeTypes[ "application/x-ruby" ] = "ruby";
+        mimeTypes[ "text/x-scala" ] = "scala";
+        mimeTypes[ "text/x-scheme" ] = "scheme";
+        //mimeTypes[ "" ] = "script_comment";
+        mimeTypes[ "application/x-shellscript" ] = "sh";
+        //mimeTypes[ "" ] = "slang";
+        mimeTypes[ "application/x-standardml" ] = "sml";
+        mimeTypes[ "text/x-rpm-spec" ] = "spec";
+        mimeTypes[ "text/x-sql" ] = "sql";
+        //mimeTypes[ "" ] = "symbols";
+        mimeTypes[ "text/x-tcl" ] = "tcl";
+        mimeTypes[ "text/x-texinfo" ] = "texinfo";
+        //mimeTypes[ "" ] = "tml*"; // tml_formatting_all, tml_formatting, tml_glue, tml_macrolinks, tml_macros, tml_macros1, tml_macros2, tml_macrosdelayed1, tml_macrosdelayed2, tml_macrosdelayed3, tml_macrotokens, tml
+        //mimeTypes[ "" ] = "todo";
+        //mimeTypes[ "" ] = "upc";
+        //mimeTypes[ "" ] = "url";
+        mimeTypes[ "text/x-vala" ] = "vala";
+        mimeTypes[ "text/x-vba" ] = "vbscript"; // not sure
+        mimeTypes[ "application/xml" ] = "xml";
+        //mimeTypes[ "" ] = "xorg";
+    }
+    
+    return mimeTypes.value( type );
 }
 
 QStringList SourceHighlightQtCodeEditor::supportedLanguages() const
