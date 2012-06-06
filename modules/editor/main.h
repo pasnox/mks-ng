4ad035689d74b17d9bbb30d7ad98e2be0dff54be
Nox P@sNox <pasnox@gmail.com>
2012-06-06 21:24:28 +0200

    Add new defines

--- modules/editor/main.h
+++ modules/editor/main.h
@@ -6,5 +6,7 @@
 #define APPLICATION_VERSION_LONG "0.1.0git"
 #define APPLICATION_DOMAIN "monkeystudio.org"
 #define APPLICATION_ORGANIZATION "SoDream"
+#define APPLICATION_MANUAL_URL "https://github.com/pasnox/mks-ng/wiki"
+#define APPLICATION_ISSUES_URL "https://github.com/pasnox/mks-ng/issues"
 
 #endif // MAIN_H
